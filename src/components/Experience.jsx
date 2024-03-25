7e10fcd77fe28004246fe39c77a47a778fd4af91
theringsofsaturn <emiliankasemi@gmail.com>
2024-03-25 02:18:16 +0100

    converted classroom.glb into jsx component

--- src/components/Experience.jsx
+++ src/components/Experience.jsx
@@ -1,6 +1,7 @@
 import { Environment, Gltf, OrbitControls } from "@react-three/drei";
 import { Canvas } from "@react-three/fiber";
 import { Emilian } from "../../public/models/Emilian";
+import { Classroom } from "../../public/models/Classroom";
 
 const Experience = () => {
   return (
@@ -8,11 +9,8 @@ const Experience = () => {
       <OrbitControls />
       <Environment preset="sunset" />
       <ambientLight intensity={0.8} color={"pink"} />
-      <Gltf
-        src="/models/classroom.glb"
-        position={[0, -8, 0]}
-        rotation={[0, Math.PI, 0]}
-      />
+
+      <Classroom position={[0, -8, 0]} rotation={[0, Math.PI, 0]} />
 
       <Emilian
         scale={(7, 7, 7)}
