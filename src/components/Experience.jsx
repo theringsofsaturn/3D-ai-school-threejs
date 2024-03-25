645f5139a6081fc273271a7c8683a60495f286a5
theringsofsaturn <emiliankasemi@gmail.com>
2024-03-25 02:07:27 +0100

    added 3D custom models

--- src/components/Experience.jsx
+++ src/components/Experience.jsx
@@ -1,14 +1,23 @@
-import { Box, OrbitControls } from "@react-three/drei";
+import { Environment, Gltf, OrbitControls } from "@react-three/drei";
 import { Canvas } from "@react-three/fiber";
 
 const Experience = () => {
   return (
     <Canvas className="canvas">
       <OrbitControls />
-      <ambientLight />
-      <Box>
-        <meshStandardMaterial />
-      </Box>
+      <Environment preset="sunset" />
+      <ambientLight intensity={0.8} color={"pink"} />
+      <Gltf
+        src="/models/classroom.glb"
+        position={[0, -8, 0]}
+        rotation={[0, Math.PI, 0]}
+      />
+      <Gltf
+        src="/models/emilian.glb"
+        scale={(7, 7, 7)}
+        position={[-11, -7.95, -14]}
+        rotation={[0, 0.5, 0]}
+      />
     </Canvas>
   );
 };
